beaa0d4b0542a65eb22901ace2f9cae4b5088146
LEBLED Arthur <arthur.lebled@reseau.eseo.fr>
2021-11-29 16:20:09 +0100

    SVM with class

--- SVM.py
+++ SVM.py
@@ -1,40 +1,43 @@
-import pandas as pd
 import numpy as np
-from sklearn.utils.validation import column_or_1d
 from sklearn.svm import SVC
-from sklearn import metrics
+from sklearn.utils.validation import column_or_1d
+from sklearn.preprocessing import StandardScaler
+class ReseauNeurones:
+    def __init__(self, donnees_train, donnees_eval):
+        self.donnees_train = donnees_train
+        self.donnees_eval = donnees_eval
+        self.svm = None
+
+        np.random.shuffle(self.donnees_train)
+        self.nombre_lignes_base = donnees_train.shape[0]
+        self.nombre_colonnes_base = donnees_train.shape[1]
+
+        self.x_train = self.donnees_train[0:round(self.nombre_lignes_base * 3 / 4), :self.donnees_train.shape[1] - 1]
+        self.y_train = self.donnees_train[0:round(self.nombre_lignes_base * 3 / 4), self.donnees_train.shape[1] - 1:]
+        self.y_train = column_or_1d(self.y_train, warn=False)
 
 
-class SVM():
-    donnees_data_frame = pd.read_csv("train.txt", delimiter=" ")
-    donnees_ensemble_total = donnees_data_frame.values
-    print(donnees_ensemble_total)
+        self.x_test = self.donnees_train[round(self.nombre_lignes_base * 3 / 4) + 1:, :self.donnees_train.shape[1] - 1]
+        self.y_test = self.donnees_train[round(self.nombre_lignes_base * 3 / 4) + 1:, self.donnees_train.shape[1] - 1:]
+        self.y_test = column_or_1d(self.y_test, warn=False)
 
-    np.random.shuffle(donnees_ensemble_total)
+        self.nombre_lignes_eval = self.donnees_eval.shape[0]
+        self.nombre_colonnes_eval = self.donnees_eval.shape[1]
 
-    nombre_lignes_base = donnees_ensemble_total.shape[0]
-    nombre_colonnes_base = donnees_ensemble_total.shape[1]
+        self.x_to_predict = self.donnees_eval[0:round(self.nombre_lignes_eval), :self.nombre_colonnes_eval - 1]
+        self.y_to_predict = self.donnees_eval[0:round(self.nombre_lignes_eval), self.nombre_colonnes_eval - 1:]
 
-    x_train = donnees_ensemble_total[0:round(nombre_lignes_base * 3 / 4), :donnees_ensemble_total.shape[1] - 1]
-    y_train = donnees_ensemble_total[0:round(nombre_lignes_base * 3 / 4), donnees_ensemble_total.shape[1] - 1:]
+    def scale(self):
+        scaler = StandardScaler(with_mean=True, with_std=True)
+        scaler.fit(self.x_train)
 
-    x_test = donnees_ensemble_total[round(nombre_lignes_base * 3 / 4) + 1:, :donnees_ensemble_total.shape[1] - 1]
-    y_test = donnees_ensemble_total[round(nombre_lignes_base * 3 / 4) + 1:, donnees_ensemble_total.shape[1] - 1:]
+        self.x_train = scaler.transform(self.x_train)
+        self.x_test = scaler.transform(self.x_test)
 
-    y_train = column_or_1d(y_train, warn=False)
-    y_test = column_or_1d(y_test, warn=False)
+        self.x_to_predict = scaler.transform(self.x_to_predict)
 
-
-    svm = SVC(gamma='auto')
-
-    print("\n\n===================\n")
-
-    # apprentissage – construction du modèle prédictif
-    svm.fit(x_train, y_train)
-
-    y_predit_test = svm.predict(x_test)
-    print(f"y_predit_test {y_predit_test}")
-
-    err = (1.0 - metrics.accuracy_score(y_test, y_predit_test)) * 100
-    print("Erreur = ", round(err, 2), "%")
-    print("\n\n===================")
+    def predict(self):
+        self.svm = SVC(gamma='auto')
+        self.svm.fit(self.x_train, self.y_train)
+        self.y_predicted = self.svm.predict(self.x_to_predict)
+        return self.y_predicted
